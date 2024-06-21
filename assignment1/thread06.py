ab15d494753be706fc7ec3d1bc0e10b8794f20a2
kong <kong08555@gmail.com>
2024-06-21 09:58:51 +0700

    thread01-thread06

--- assignment1/thread06.py
+++ assignment1/thread06.py
@@ -1,1 +1,26 @@
-# Working With Many Threads
+# Woimport logging
+import logging
+import threading
+import time
+
+def thread_function(name):
+    logging.info("Thread %s: startng", name)
+    time.sleep(2)
+    logging.info("Thread %s: finishing", name)
+    
+if __name__=="__main__":
+    format = "%(asctime)s: %(message)s"
+    logging.basicConfig(format=format, level=logging.INFO,
+                        datefmt="%H:%M:%S")
+    
+    threads = list()
+    for index in range(3):
+        logging.info("Main  : create and start thread %d.", index)
+        x = threading.Thread(target=thread_function, args=(index,))
+        threads.append(x)
+        x.start()
+
+    for index, thread in enumerate(threads):
+        logging.info("Main  :before joinging thread %d.", index)
+        thread.join()
+        logging.info("Main  : thread %d done", index)
