151dae6625133c9ddd0d3d6375188426e415e2e3
kong <kong08555@gmail.com>
2024-07-19 10:15:48 +0700

    assign-6-bf

--- assignment06/breakfast02.py
+++ assignment06/breakfast02.py
@@ -16,12 +16,12 @@ class Toast:
 class Juice:
     pass
 
-async def PourCoffee():
-    print(f"{time.ctime()} - Begin pour coffee...")
-    await asyncio.sleep(2)  
-    print(f"{time.ctime()} - Finish pour coffee...")
-    print(f"{time.ctime()} - >>>>>>>>>> Coffee is ready\n")
-    return Coffee()
+def PourCoffee():
+     print(f"{time.ctime()} - Begin pour coffee...")
+     time.sleep(2)  
+     print(f"{time.ctime()} - Finish pour coffee...")
+     print(f"{time.ctime()} - >>>>>>>>>> Coffee is ready\n")
+     return Coffee()
     #return value to class  to indicate process this function complete
 
 async def ApplyButter():
@@ -57,15 +57,15 @@ async def ToastBread(slices): #slices in number of bread to want toast
     print(f"{time.ctime()} - >>>>>>>>>>> Toast are ready\n")
     return Toast()
 
-async def PourJuice():
+def PourJuice():
     print(f"{time.ctime()} - Begin pour juice...")
-    await asyncio.sleep(1)  
+    time.sleep(1)  
     print(f"{time.ctime()} - Finish pour juice...")
     return Juice()
 
 async def main():
-    
-    await PourCoffee()
+
+    PourCoffee() # await func is waiting this func finish and continue
     tasks = [asyncio.create_task(FryEggs(2)),
              asyncio.create_task(FryBacon()), 
              asyncio.create_task(ToastBread(2))]
@@ -73,10 +73,11 @@ async def main():
     await asyncio.gather(*tasks) 
     #receive all task in array tasks to working together and wait until every task is finished.
     print(f"{time.ctime()} - >>>>>>>> Nearly to finished...")
-    await PourJuice()
+    PourJuice()
 
 if __name__ == "__main__":
     start_cooking = time.perf_counter() #Record the current time in variable.
     asyncio.run(main())
-    elapsed = time.perf_counter() - start_cooking
+    elapsed = time.perf_counter() - start_cooking 
+    #Calculate the total running time of the program by subtracting the current time.
     print(f"{time.ctime()} - Breakfast cooked in" ,elapsed, "seconds.")
